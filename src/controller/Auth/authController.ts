5d7dbacb29a717c4b1c73380825dbe8082ce9d3b
jahfalrizqi <jahfalrizqi14@gmail.com>
2025-01-18 06:34:31 +0700

    adjust decrypted password for login authentication

--- src/controller/Auth/authController.ts
+++ src/controller/Auth/authController.ts
@@ -3,7 +3,7 @@ import { User } from "../../model/User";
 import { AppDataSource } from "../../data-source";
 import { JwtPayload } from "../../types/JwtPayload";
 import { createJwtToken } from "../../utils/createJwtToken";
-import bcrypt from 'bcryptjs';
+import { decrypt } from "../../utils/CryptoData";
 
 const { successResponse, errorResponse, validationResponse } = require('../../utils/response')
 
@@ -22,41 +22,41 @@ export const fetch = async (req: Request, res: Response) => {
 }
 
 
-export const login = async (req: Request, res: Response) => {
-    try {
-        const { userName,password } = req.body
-
-        const user = await userRepository.findOne({
-            where: {
-                userName: userName,
-            }
-        })
-
-        if (!user) {
-            return res.status(409).send(errorResponse('Incorect userName or password ', 409))
-        }
-
-
-              // Verifikasi password
-              const isPasswordValid = await bcrypt.compare(password, user.password);
-              if (!isPasswordValid) {
-                  return res.status(409).send(errorResponse('Incorrect userName or password', 409));
-              }
-
-
-        const jwtPayload: JwtPayload = {
-            id: user.id,
-            userName: user.userName,
-            password : user.password,
+export const login = async (req: Request, res: Response) => {  
+    try {  
+        const { userName, password } = req.body;  
+  
+        const user = await userRepository.findOne({  
+            where: {  
+                userName: userName,  
+            }  
+        });  
+  
+        if (!user) {  
+            return res.status(409).send(errorResponse('Incorrect userName or password', 409));  
+        }  
+  
+        // Dekripsi password yang disimpan  
+        const decryptedPassword = decrypt(user.password);  
+  
+        // Verifikasi password  
+        if (password !== decryptedPassword) {  
+            return res.status(409).send(errorResponse('Incorrect userName or password', 409));  
+        }  
+  
+        const jwtPayload: JwtPayload = {  
+            id: user.id,  
+            userName: user.userName, 
+            password : user.password, 
             createdAt: user.createdAt,
-        }
-        console.log(jwtPayload)
-
-        const token = createJwtToken(jwtPayload)
-        const data = { user, token }
-
-        return res.status(200).send(successResponse("Login Success", { data: data }, res.statusCode))
-    } catch (error) {
-        return res.status(400).send(errorResponse(error, 400))
-    }
-}
+        };  
+        console.log(jwtPayload);  
+  
+        const token = createJwtToken(jwtPayload);  
+        const data = { user, token };  
+  
+        return res.status(200).send(successResponse("Login Success", { data: data }, res.statusCode));  
+    } catch (error) {  
+        return res.status(400).send(errorResponse(error, 400));  
+    }  
+}  
