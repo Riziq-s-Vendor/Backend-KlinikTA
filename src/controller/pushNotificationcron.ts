9b8193b59aee8d2311a47b94939fdf02887ca3cb
jahfalrizqi <jahfalrizqi14@gmail.com>
2025-01-30 03:38:21 +0700

    fix export cronjob without real notification

--- src/controller/pushNotificationcron.ts
+++ src/controller/pushNotificationcron.ts
@@ -1,26 +1,12 @@
-import express from 'express';
 import cron from 'node-cron';
-import dotenv from 'dotenv';
 
-// Memuat variabel lingkungan dari file .env
-dotenv.config();
+export const startCronJob = () => {
+    console.log("Menyiapkan cron job...");
 
-const app = express();
-const PORT = process.env.APP_PORT || 3000;
+    // Contoh cron job yang berjalan setiap menit
+    cron.schedule('* * * * *', () => {
+        console.log('Cron job berjalan setiap menit: push notif!');
+    });
 
-console.log("APP_PORT:", process.env.APP_PORT); // Pastikan ini mencetak 5000
-
-// Contoh cron job yang berjalan setiap menit
-cron.schedule('* * * * *', () => {
-    console.log('Cron job berjalan setiap menit: Push Notif!');
-});
-
-console.log("Cron job di-schedule"); // Tambahkan log di sini
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+    console.log("Cron job di-schedule");
+};
