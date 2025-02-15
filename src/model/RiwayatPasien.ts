9b3708986c2f3a31aca390194f7972022b17ee0b
jahfalrizqi <jahfalrizqi14@gmail.com>
2025-02-16 04:17:01 +0700

    refactor: fix value status rm

--- src/model/RiwayatPasien.ts
+++ src/model/RiwayatPasien.ts
@@ -7,7 +7,7 @@ import { peminjamanRekamMedis } from "./peminjamanRekamMedis";
 export enum StatusRM {
     TERSEDIA = 'TERSEDIA',
     DIPINJAM = 'DIPINJAM',
-    TERLAMBATDIKEMBALIKAN = ' TERLAMBATDIKEMBALIKAN',
+    TERLAMBATDIKEMBALIKAN = 'TERLAMBATDIKEMBALIKAN',
 
 }
 
