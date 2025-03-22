9bec46a99b5632c52450c29ca762baa02fced374
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-03-22 07:32:34 +0700

    feat : new model auutonamnesis Rekam Medis for preparing repeater input on create and update rekam medis

---
+++ src/model/AutonamnesisRekamMedis.ts
@@ -0,0 +1,64 @@
+import { Entity,OneToMany,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,DeleteDateColumn, JoinColumn, ManyToOne } from "typeorm";
+import { IsDate, IsNumber, IsString,IsUppercase } from "class-validator";
+import { RiwayatPasien } from "./RiwayatPasien";
+
+
+
+@Entity()
+export class autonamnesisRekamMedis{
+
+    @PrimaryGeneratedColumn('uuid')
+    public id : string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public ku: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public kt: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public rpd: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public rpo: string
+
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public rpk: string
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+    //preparing for repeater input
+    // @OneToMany(() => RiwayatPasien,(rekamMedis) => rekamMedis.autonamnesis)
+    // public rekamMedis : RiwayatPasien
+
+
+}
+
