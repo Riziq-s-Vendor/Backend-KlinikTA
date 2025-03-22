41568aed250fd7188b181e555ecba422bffce71d
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-03-22 07:33:47 +0700

    feat : new model planning rekam medis for prepare repeater input on rekam medis

---
+++ src/model/planningRekamMedis.ts
@@ -0,0 +1,68 @@
+import { Entity,OneToMany,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,DeleteDateColumn, JoinColumn, ManyToOne } from "typeorm";
+import { IsDate, IsNumber, IsString,IsUppercase } from "class-validator";
+import { RiwayatPasien } from "./RiwayatPasien";
+
+export enum Edukasi {
+    POLAMAKAN = 'POLAMAKAN',
+    POLAAKTIFITAS = 'POLAAKTIFITAS',
+}
+
+@Entity()
+export class planningRekamMedis{
+
+    @PrimaryGeneratedColumn('uuid')
+    public id : string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public therapy: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public eso: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public rencanaPemeriksaanPenunjang: string
+
+    @Column({
+         type: 'enum',
+        enum: Edukasi,
+    })
+    @IsString()
+    public rencanaEdukasi: Edukasi
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public rencanaRujukan: string
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+        //preparing for repeater input
+
+
+    // @OneToMany(() => RiwayatPasien,(rekamMedis) => rekamMedis.planning)
+    // public rekamMedis : RiwayatPasien
+
+
+
+}
