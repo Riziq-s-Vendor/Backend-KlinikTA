7744f16e5d00869ad72e02cb939e587668c533c3
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-04-12 11:16:14 +0700

    refactor : fix props riwayat pasiens

--- src/model/RiwayatPasien.ts
+++ src/model/RiwayatPasien.ts
@@ -29,147 +29,147 @@ export class RiwayatPasien{
     public id : string
 
 
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public lamaPenyakit: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public lamaPenyakit: string
 
     
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public lain_lain: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public lain_lain: string
 
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public waktuPermeriksaan: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public waktuPermeriksaan: string
 
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public fisik: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public fisik: string
 
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public lain_lainHasilPemeriksaan: string
-
-    
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public laboratorium: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public lain_lainHasilPemeriksaan: string
 
     
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public radiologi: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public keadaanKeluarRS: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public prognosa: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public kapanPenyakitDahulu: string
-
-        @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public pengobatan: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public faktorEtimologi: string
-
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public diagnosaAkhir: string
-
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public masalahDihadapi: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public konsultasi: string
-
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public pengobatanTindakan: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public laboratorium: string
 
     
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public perjalananPeyakit: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public radiologi: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public keadaanKeluarRS: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public prognosa: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public kapanPenyakitDahulu: string
+
+    //     @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public pengobatan: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public faktorEtimologi: string
+
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public diagnosaAkhir: string
+
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public masalahDihadapi: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public konsultasi: string
+
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public pengobatanTindakan: string
 
     
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public sebabMeninggal: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public perjalananPeyakit: string
 
     
-    @Column({
-        default: null,
-        nullable: true
-    })
-    @IsString()
-    public usulTidakLanjut: string
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public sebabMeninggal: string
+
+    
+    // @Column({
+    //     default: null,
+    //     nullable: true
+    // })
+    // @IsString()
+    // public usulTidakLanjut: string
 
     
     @Column({
