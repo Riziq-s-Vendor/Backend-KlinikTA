b7cebc3cf640f0647151486db63c5e79ea7b9074
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-04-29 21:44:40 +0700

    refactor : fix model riwayat pasien

--- src/model/RiwayatPasien.ts
+++ src/model/RiwayatPasien.ts
@@ -28,149 +28,6 @@ export class RiwayatPasien{
     @PrimaryGeneratedColumn('uuid')
     public id : string
 
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public lamaPenyakit: string
-
-    
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public lain_lain: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public waktuPermeriksaan: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public fisik: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public lain_lainHasilPemeriksaan: string
-
-    
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public laboratorium: string
-
-    
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public radiologi: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public keadaanKeluarRS: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public prognosa: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public kapanPenyakitDahulu: string
-
-    //     @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public pengobatan: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public faktorEtimologi: string
-
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public diagnosaAkhir: string
-
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public masalahDihadapi: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public konsultasi: string
-
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public pengobatanTindakan: string
-
-    
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public perjalananPeyakit: string
-
-    
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public sebabMeninggal: string
-
-    
-    // @Column({
-    //     default: null,
-    //     nullable: true
-    // })
-    // @IsString()
-    // public usulTidakLanjut: string
-
     
     @Column({
         type: 'enum',
@@ -230,12 +87,6 @@ export class RiwayatPasien{
         @IsString()
         public rpk: string
 
-        @Column({
-            default: null,
-            nullable: true
-        })
-        @IsString()
-        public vitalSignSensorium: string
     
         @Column({
             default: null,
@@ -363,25 +214,6 @@ export class RiwayatPasien{
     @OneToMany (() => peminjamanRekamMedis, (peminjamanRekamMedis) => peminjamanRekamMedis.RiwayatPasiens)
     public peminjamanRekamMedis : peminjamanRekamMedis
 
-    //preparing for repeater input
-
-    // @ManyToOne(() => autonamnesisRekamMedis,(autonamnesis) => autonamnesis.rekamMedis)
-    // @JoinColumn()
-    // public autonamnesis : autonamnesisRekamMedis
-
-    
-    // @ManyToOne(() => objektifRekamMedis,(objekRekamMedis) => objekRekamMedis.rekamMedis)
-    // @JoinColumn()
-    // public objekRekamMedis : objektifRekamMedis
-
-    // @ManyToOne(() => assessmentRekamMedis,(assessment) => assessment.rekamMedis)
-    // @JoinColumn()
-    // public assessment : assessmentRekamMedis
-
-    
-    // @ManyToOne(() => planningRekamMedis,(planning) => planning.rekamMedis)
-    // @JoinColumn()
-    // public planning : planningRekamMedis
 
 
 
