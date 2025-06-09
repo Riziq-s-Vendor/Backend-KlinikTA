f2305a8050f72c8bed3e16bcd0bc2f19ffd3783c
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-06-09 15:42:50 +0700

    refactor : fix create jwt token for deploymnet

--- src/utils/createJwtToken.ts
+++ src/utils/createJwtToken.ts
@@ -7,3 +7,20 @@ export const createJwtToken = (payload: JwtPayload): string => {
     expiresIn: process.env.JWT_EXPIRATION! // Tambahkan non-null assertion di sini
   })
 }
+
+
+// import jwt, { SignOptions } from 'jsonwebtoken'
+// import { JwtPayload } from '../types/JwtPayload'
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// export const createJwtToken = (payload: JwtPayload): string => {
+//   const options: SignOptions = {
+//     expiresIn: process.env.JWT_EXPIRATION ? 
+//       parseInt(process.env.JWT_EXPIRATION) : 
+//       2592000 // default 1 jam dalam detik
+//   }
+
+//   return jwt.sign(payload, process.env.JWT_SECRET!, options)
+// }
